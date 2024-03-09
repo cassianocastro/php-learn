4bfae1b4a8283f6557bc0034bed9d0fa4a604217
Cassiano Castro <development.castro@gmail.com>
2024-03-09 00:51:18 -0300

    Perf: removed strings concatenation

--- model/DataBaseAccess/mysqli/003.prepare.mysql.php
+++ model/DataBaseAccess/mysqli/003.prepare.mysql.php
@@ -1,17 +1,12 @@
 <?php
-
 $mysqli = new mysqli('localhost', 'root', '', 't308');
 
 if ( $mysqli->connect_error )
 {
-    die(
-        'Connection Error ('
-        . $mysqli->connect_errno . ') '
-        . $mysqli->connect_error
-    );
+    die("Connection error({$mysqli->connect_errno}): {$mysqli->connect_error}");
 }
 
-$query = $mysqli->prepare('SELECT * FROM alunos WHERE id = ? ');
+$query = $mysqli->prepare('SELECT * FROM alunos WHERE id = ?');
 
 $query->bind_param('i', 1);
 
