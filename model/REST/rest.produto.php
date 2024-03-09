d2b177e36e7b8670c5cf16976606031047824d8d
Cassiano Castro <development.castro@gmail.com>
2024-03-09 01:32:18 -0300

    Style: format and indent adjust

--- model/REST/rest.produto.php
+++ model/REST/rest.produto.php
@@ -1,16 +1,22 @@
 <?php
-try{
+try
+{
 	include 'connection.php';
-	$method_name = $_SERVER["REQUEST_METHOD"];
-	echo $method_name;
 
-	if($_SERVER["REQUEST_METHOD"]) {
-		switch ($method_name) {
+	$method_name = $_SERVER["REQUEST_METHOD"];
+
+    echo $method_name;
+
+	if ( $_SERVER["REQUEST_METHOD"] )
+    {
+		switch ( $method_name )
+        {
 			case 'GET':
 				$qry    = "SELECT * from produto";
 				$result = mysqli_query($conn, $qry);
 
-				while ($row = mysqli_fetch_row($result)) {
+				while ( $row = mysqli_fetch_row($result) )
+                {
 					$temp_cat[] = array(
 						"produto_id"    =>$row[0],
 						"produto_nome"  =>$row[1],
@@ -24,26 +30,30 @@ try{
 					"message" => "success",
 					"result"  => $temp_cat
 				);
-			break;
+			    break;
 			case 'POST':
 				$name  = $_REQUEST['produto_nome'];
 				$price = $_REQUEST['produto_preco'];
 				$qty   = $_REQUEST['produto_qtd'];
 				$qry   = "INSERT INTO produto(produto_nome,produto_preco,produto_qtd) values('$name','$price','$qty')";
 
-				if(mysqli_query($conn, $qry))
+				if ( mysqli_query($conn, $qry) )
+                {
 					$data = array(
 						"status"  => "1",
 						"message" => "success",
 						"result"  => "produto add successfully"
 					);
+                }
 				else
+                {
 					$data = array(
 						"status"  => "1",
 						"message" => "success",
 						"result"  => "Something wrong!!!"
 					);
-			break;
+                }
+			    break;
 			case 'PUT':
 				$id    = $_REQUEST['produto_id'];
 				$name  = $_REQUEST['produto_nome'];
@@ -51,47 +61,59 @@ try{
 				$qty   = $_REQUEST['produto_qtd'];
 				$qry   = "UPDATE produto SET produto_nome='".$name."', produto_preco='".$price."',produto_qtd='".$qty."' where produto_id='".$id."' ";
 
-				if(mysqli_query($conn, $qry))
+				if ( mysqli_query($conn, $qry) )
+                {
 					$data = array(
 						"status"=>"1",
 						"message"=>"success",
 						"result"=>"produto Update successfully"
 					);
+                }
 				else
+                {
 					$data = array(
 						"status"=>"1",
 						"message"=>"success",
 						"result"=>"Something wrong!!!"
 					);
-			break;
+                }
+			    break;
 			case 'DELETE':
 				$id  = $_REQUEST['produto_id'];
 				$qry = "delete from produto where produto_id='".$id."'";
 
-				if(mysqli_query($conn, $qry))
+				if ( mysqli_query($conn, $qry) )
+                {
 					$data = array(
 						"status"=>"1",
 						"message"=>"success",
 						"result"=>"produto Update successfully"
 					);
+                }
 				else
+                {
 					$data = array(
 						"status"=>"1",
 						"message"=>"success",
 						"result"=>"Something wrong!!!"
 					);
-			break;
+                }
+			    break;
 		}
+
 		echo json_encode($data);
 	}
-	else{
+	else
+    {
 		$data=array(
 			"status"  => "0",
 			"message" => "Please enter proper request method!!"
 		);
+
 		echo json_encode($data);
 	}
 }
-catch(Exception $e) {
+catch ( Exception $e )
+{
 	echo 'Caught exception: ', $e->getMessage(), "\n";
 }
