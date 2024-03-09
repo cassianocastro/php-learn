57b22c6447d327a7af36f7141e238af8ddd0cd40
Cassiano Castro <development.castro@gmail.com>
2024-03-09 01:27:12 -0300

    Style: format adjust

--- model/REST/connection.php
+++ model/REST/connection.php
@@ -1,23 +1,18 @@
 <?php
 /*
-CREATE TABLE IF NOT EXISTS `produto` (
-`produto_id` INT NOT NULL AUTO_INCREMENT ,
-`produto_nome` VARCHAR(30) NOT NULL ,
-`produto_prreco` INT NOT NULL ,
-`produto_qtd` INT NOT NULL ,
-PRIMARY KEY (`produto_id`)) ENGINE = InnoDB;
-*/
-$conn = new mysqli(
-    'localhost',
-    'root',
-    '',
-    't308'
-);
+CREATE TABLE IF NOT EXISTS produto (
+    produto_id     INT NOT NULL AUTO_INCREMENT,
+    produto_nome   VARCHAR(30) NOT NULL,
+    produto_prreco INT NOT NULL,
+    produto_qtd    INT NOT NULL,
 
-if ($conn->connect_error) {
-    die(
-        'Connect Error ('
-        . $conn->connect_errno . ') '
-        . $conn->connect_error
-    );
+    PRIMARY KEY(produto_id)
+) ENGINE = InnoDB;
+*/
+
+$conn = new mysqli('localhost', 'root', '', 't308');
+
+if ( $conn->connect_error )
+{
+    die('Connect Error (' . $conn->connect_errno . ') ' . $conn->connect_error);
 }
