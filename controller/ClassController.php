31b68c7115b0ae9b027c17120f4c4d27200bc9fb
Cassiano Castro <development.castro@gmail.com>
2024-03-10 19:26:58 -0300

    Refactor: surrounded PHP code in functions

--- controller/ClassController.php
+++ controller/ClassController.php
@@ -1,132 +1,148 @@
 <?php
-
-// Section: Trabalhando com Classes em PHP
-
-ini_set("display_errors", true);
-error_reporting(E_ALL);
 require_once '../model/POO/Classes/Classes.php';
 
-$pessoa = new Pessoa();
-$pessoa->idade = 18;
-echo "Objeto: Chamando método mostra:" . PHP_EOL;
-$pessoa->mostra();
-?>
+/**
+ * Section: Trabalhando com Classes em PHP
+ */
+function workWithClasses(): void
+{
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
 
-<?php
+    $pessoa = new Pessoa();
+    $pessoa->idade = 18;
 
-// Section: Criando Array de Objetos
+    echo "Objeto: Chamando método mostra:" . PHP_EOL;
 
-print "Array de Objetos:<br>";
+    $pessoa->mostra();
+}
 
-$pessoas = [];
-$pessoas[0] = new Pessoa();
-$pessoas[0]->idade = 18;
-$pessoas[1] = new Pessoa();
-$pessoas[1]->idade = 22;
-$pessoas[2] = new Pessoa();
+/**
+ * Section: Criando Array de Objetos
+ */
+function createArrayOfObjects(): void
+{
+    print "Array de Objetos:<br>";
 
-$pessoas[0]->mostra();
-$pessoas[1]->mostra();
-$pessoas[2]->mostra();
-?>
+    $pessoas = [];
+    $pessoas[0] = new Pessoa();
+    $pessoas[0]->idade = 18;
+    $pessoas[1] = new Pessoa();
+    $pessoas[1]->idade = 22;
+    $pessoas[2] = new Pessoa();
 
-<?php
+    $pessoas[0]->mostra();
+    $pessoas[1]->mostra();
+    $pessoas[2]->mostra();
+}
 
-// Section: Encapsulamento
+/**
+ * Section: Encapsulamento
+ */
+function encapsulating(): void
+{
+    $objeto = new ClasseExemplo();
 
-$objeto = new ClasseExemplo();
+    echo <<<HTML
+        Chamando setAtributo...
+        {$objeto->setAtributo("Novo valor 2")}
+        Chamando getAtributo: {$objeto->getAtributo()}
+    HTML;
+}
 
-echo <<<HTML
-    Chamando setAtributo...
-    {$objeto->setAtributo("Novo valor 2")}
-    Chamando getAtributo: {$objeto->getAtributo()}
-HTML;
-?>
+/**
+ * Section: Classes aninhadas
+ */
+function classesAninhadas(): void
+{
+    $cc = new Conta();
+    $cc->setTitular("Ana");
 
-<?php
+    echo "<p>Titular: {$cc->getTitular()}</p>";
+}
 
-// Section: Classes aninhadas
+/**
+ * Section: Constructors e destructors
+ */
+function constructorsAndDestructors(): void
+{
+    $objeto = new MinhaClasse("Borboleta");
+    $objeto = null; // chama o destructor
+}
 
-$cc = new Conta();
-$cc->setTitular("Ana");
+/**
+ * Section: Herança: Reescrita de constructors e destructors
+ */
+function rewriteContructors(): void
+{
+    echo "<h4>Classe A</h4>";
+    echo "Inicializando classe A: " . PHP_EOL;
+    $objA = new ClasseA();
 
-echo "<p>Titular: {$cc->getTitular()}</p>";
-?>
+    echo "Chamando método mostraA(): " . PHP_EOL;
+    $objA->mostraApublica();
+    $objA = null;
 
-<?php
+    echo "<h4>Classe B</h4>";
+    echo "Etapa1: Inicializando..." . PHP_EOL;
+    $objB = new ClasseB();
 
-// Section: Constructors e destructors
+    echo "Etapa2: Chamando..." . PHP_EOL;
+    $objB->mostraB();
 
-$objeto = new MinhaClasse("Borboleta");
-$objeto = null; // chama o destructor
-?>
+    echo "Etapa3: Destruindo..." . PHP_EOL;
+    // $objB->mostraA();
+    $objB = null;
 
-<?php
+    echo "<h4>Classe C</h4>";
+    $objC = new ClasseC(); // Chama os construtores da ClasseC e ClasseA
+    $objC->mostraApublica();
+    $objC = null;
 
-// Section: Herança: Reescrita de constructors e destructors
+    echo "<h4>Classe D</h4>";
+    $objD = new ClasseD(); // Chama os construtor/destructor da ClasseA
+    $objD->mostraApublica();
+    $objD = null;
+}
 
-echo "<h4>Classe A</h4>";
-echo "Inicializando classe A: " . PHP_EOL;
-$objA = new ClasseA();
-echo "Chamando método mostraA(): " . PHP_EOL;
-$objA->mostraApublica();
-$objA = null;
+/**
+ * Section: Herança Complexa: Controle de instâncias
+ */
+function instancesControl(): void
+{
+    $o1 = new ChildObject();
+    $o2 = new ChildObject2();
+    $x  = $o1->getInstanceGUID();
+    $y  = $o2->getInstanceGUID();
+    $instanceCount = SimpleObject::getInstanceCount();
 
-echo "<h4>Classe B</h4>";
-echo "Etapa1: Inicializando..." . PHP_EOL;
-$objB = new ClasseB();
-echo "Etapa2: Chamando..." . PHP_EOL;
-$objB->mostraB();
-echo "Etapa3: Destruindo..." . PHP_EOL;
-//$objB->mostraA();
-$objB = null;
+    echo <<<HTML
+    <table>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>GUID</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>{$o1->getInstanceId()}</td>
+            <td>{$o1->getInstanceGUID()}</td>
+        </tr>
+        <tr>
+            <td>{$o2->getInstanceId()}</td>
+            <td>{$o2->getInstanceGUID()}</td>
+        </tr>
+        </tbody>
+    </table>
 
-echo "<h4>Classe C</h4>";
-$objC = new ClasseC(); //Chama os construtores da ClasseC e ClasseA
-$objC->mostraApublica();
-$objC = null;
+    SimpleObjects: $instanceCount\n\n
+    HTML;
 
-echo "<h4>Classe D</h4>";
-$objD = new ClasseD(); //Chama os construtor/destructor da ClasseA
-$objD->mostraApublica();
-$objD = null;
-?>
+    print_r(SimpleObject::getInstance($x));
+    print_r(SimpleObject::getInstance($y));
+    print_r("\nNULL:", SimpleObject::getInstance(null));
 
-<?php
-
-// Section: Herança Complexa: Controle de instâncias
-
-$o1 = new ChildObject();
-$o2 = new ChildObject2();
-$x  = $o1->getInstanceGUID();
-$y  = $o2->getInstanceGUID();
-$instanceCount = SimpleObject::getInstanceCount();
-echo <<<HTML
-<table>
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>GUID</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>{$o1->getInstanceId()}</td>
-        <td>{$o1->getInstanceGUID()}</td>
-    </tr>
-    <tr>
-        <td>{$o2->getInstanceId()}</td>
-        <td>{$o2->getInstanceGUID()}</td>
-    </tr>
-    </tbody>
-</table>
-SimpleObjects: $instanceCount\n\n
-HTML;
-
-print_r(SimpleObject::getInstance($x));
-print_r(SimpleObject::getInstance($y));
-print_r("\nNULL:", SimpleObject::getInstance(null));
-
-echo "\nAll Instances:" . PHP_EOL;
-print_r(SimpleObject::getAllInstances());
-?>
+    echo "\nAll Instances:" . PHP_EOL;
+    print_r(SimpleObject::getAllInstances());
+}
