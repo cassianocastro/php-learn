792880a0817fc845c38a724ac0e95017e9187c22
Cassiano Castro <development.castro@gmail.com>
2024-03-10 18:47:32 -0300

    Added PHP code

---
+++ controller/ActionController.php
@@ -0,0 +1,18 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+function show()
+{
+    echo "Hi " . htmlspecialchars($_POST['name']) . ".";
+    echo "You are " . (int) $_POST['age'] . " years old.";
+}
+
+if ( isset($_POST["action"]) )
+{
+    print_r($_POST);
+    // clicked();
+    $_POST["action"]();
+}
+?>
