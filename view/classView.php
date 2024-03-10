914449b9436b8d5fef7f07305c54a8fef3c1cc98
Cassiano Castro <development.castro@gmail.com>
2024-03-10 19:28:16 -0300

    Refactor: moved PHP content to controller/ClassController.php file

--- view/classView.php
+++ view/classView.php
@@ -1,3 +1,5 @@
+<?php require_once __DIR__ . '/../controller/ClassController.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR" dir="ltr">
 <head>
@@ -18,16 +20,7 @@
 
       <section>
         <pre>
-          <?php
-          ini_set("display_errors", true);
-          error_reporting(E_ALL);
-          require_once '../model/POO/Classes/Classes.php';
-
-          $pessoa = new Pessoa();
-          $pessoa->idade = 18;
-          echo "Objeto: Chamando método mostra:" . PHP_EOL;
-          $pessoa->mostra();
-          ?>
+          <?= workWithClasses() ?>
         </pre>
       </section>
 
@@ -37,20 +30,7 @@
         <h3>Criando Array de Objetos</h3>
 
         <pre>
-          <?php
-          print "Array de Objetos:<br>";
-
-          $pessoas = [];
-          $pessoas[0] = new Pessoa();
-          $pessoas[0]->idade = 18;
-          $pessoas[1] = new Pessoa();
-          $pessoas[1]->idade = 22;
-          $pessoas[2] = new Pessoa();
-
-          $pessoas[0]->mostra();
-          $pessoas[1]->mostra();
-          $pessoas[2]->mostra();
-          ?>
+          <?= createArrayOfObjects() ?>
         </pre>
       </section>
 
@@ -60,15 +40,7 @@
         <h3>Encapsulamento</h3>
 
         <pre>
-          <?php
-          $objeto = new ClasseExemplo();
-
-          echo <<<HTML
-            Chamando setAtributo...
-            {$objeto->setAtributo("Novo valor 2")}
-            Chamando getAtributo: {$objeto->getAtributo()}
-          HTML;
-          ?>
+          <?= encapsulating() ?>
         </pre>
       </section>
 
@@ -78,11 +50,7 @@
         <h3>Classes aninhadas</h3>
 
         <pre>
-          <?php
-          $cc = new Conta();
-          $cc->setTitular("Ana");
-          echo "<p>Titular: {$cc->getTitular()}</p>";
-          ?>
+          <?= classesAninhadas() ?>
         </pre>
       </section>
 
@@ -92,10 +60,7 @@
         <h3>Constructors e destructors</h3>
 
         <pre>
-          <?php
-          $objeto = new MinhaClasse("Borboleta");
-          $objeto = null; // chama o destructor
-          ?>
+          <?= constructorsAndDestructors() ?>
         </pre>
       </section>
 
@@ -105,33 +70,7 @@
         <h3>Herança: Reescrita de constructors e destructors</h3>
 
         <pre>
-          <?php
-          echo "<h4>Classe A</h4>";
-          echo "Inicializando classe A: " . PHP_EOL;
-          $objA = new ClasseA();
-          echo "Chamando método mostraA(): " . PHP_EOL;
-          $objA->mostraApublica();
-          $objA = null;
-
-          echo "<h4>Classe B</h4>";
-          echo "Etapa1: Inicializando..." . PHP_EOL;
-          $objB = new ClasseB();
-          echo "Etapa2: Chamando..." . PHP_EOL;
-          $objB->mostraB();
-          echo "Etapa3: Destruindo..." . PHP_EOL;
-          //$objB->mostraA();
-          $objB = null;
-
-          echo "<h4>Classe C</h4>";
-          $objC = new ClasseC(); //Chama os construtores da ClasseC e ClasseA
-          $objC->mostraApublica();
-          $objC = null;
-
-          echo "<h4>Classe D</h4>";
-          $objD = new ClasseD(); //Chama os construtor/destructor da ClasseA
-          $objD->mostraApublica();
-          $objD = null;
-          ?>
+          <?= rewriteContructors() ?>
         </pre>
       </section>
 
@@ -141,41 +80,7 @@
         <h3>Herança Complexa: Controle de instâncias</h3>
 
         <pre>
-          <?php
-          $o1 = new ChildObject();
-          $o2 = new ChildObject2();
-          $x  = $o1->getInstanceGUID();
-          $y  = $o2->getInstanceGUID();
-          $instanceCount = SimpleObject::getInstanceCount();
-          echo <<<HTML
-            <table>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>GUID</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{$o1->getInstanceId()}</td>
-                  <td>{$o1->getInstanceGUID()}</td>
-                </tr>
-                <tr>
-                  <td>{$o2->getInstanceId()}</td>
-                  <td>{$o2->getInstanceGUID()}</td>
-                </tr>
-              </tbody>
-            </table>
-          SimpleObjects: $instanceCount\n\n
-          HTML;
-
-          print_r(SimpleObject::getInstance($x));
-          print_r(SimpleObject::getInstance($y));
-          print_r("\nNULL:", SimpleObject::getInstance(null));
-
-          echo "\nAll Instances:" . PHP_EOL;
-          print_r(SimpleObject::getAllInstances());
-          ?>
+          <?= instancesControl() ?>
         </pre>
       </section>
     </article>
