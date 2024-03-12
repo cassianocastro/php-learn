5acadd19a5ed1c6664072a95cde8fea2cce6b3e5
Cassiano Castro <development.castro@gmail.com>
2024-03-11 23:22:14 -0300

    Refactor: surrounded PHP code snippets in functions

--- controller/VariablesController.php
+++ controller/VariablesController.php
@@ -1,81 +1,90 @@
 <?php
-// Section: Demonstração do Uso de Variáveis
+require_once '../model/Functions/functions.php';
 
 ini_set("display_errors", true);
 error_reporting(E_ALL);
 
-$a = "5"; // string
-echo "Valor de \$a: ", $a + 2 . PHP_EOL; // 7, integer
+/**
+ * Section: Demonstração do Uso de Variáveis
+ */
+function demonstrateVars(): void
+{
+    $a = "5"; // string
+    echo "Valor de \$a: ", $a + 2 . PHP_EOL; // 7, integer
 
-$b = $a + '5 carros.';
-echo "Valor de \$b: ", $b . PHP_EOL;
-// 10, integer  (conversão implícita)
+    $b = $a + '5 carros.';
 
-$a = 1;
-echo "Valor de \$a: ", ++$a . PHP_EOL;  // Incrementamos 1 e retornamos o valor
-echo "Valor de \$a: ", $a++ . PHP_EOL;  // Retornamos, depois incrementamos 1
-echo "Valor de \$a: ", --$a . PHP_EOL;  // Decrementamos 1 e retornamos o valor
-echo "Valor de \$a: ", $a-- . PHP_EOL;  // Retornamos, depois decrementamos
-$a = 4;
-$a -= 2;     //  $a = $a - 2;
-$a *= 2;     //  $a = $a * 2;
-$a /= 2;     //  $a = $a / 2;
-$a += 2;     //  $a = $a + 2;
-echo "Valor de \$a: ", $a . PHP_EOL;
-?>
+    echo "Valor de \$b: ", $b . PHP_EOL;
+    // 10, integer (conversão implícita)
 
-<?php
-// Section: Demonstração do Uso de Funções
+    $a = 1;
 
-require_once '../model/Functions/functions.php';
+    echo "Valor de \$a: ", ++$a . PHP_EOL; // Incrementamos 1 e retornamos o valor
+    echo "Valor de \$a: ", $a++ . PHP_EOL; // Retornamos, depois incrementamos 1
+    echo "Valor de \$a: ", --$a . PHP_EOL; // Decrementamos 1 e retornamos o valor
+    echo "Valor de \$a: ", $a-- . PHP_EOL; // Retornamos, depois decrementamos
 
-$x1 = 2;
-$y1 = 3;
-$z1 = calcular($x1, $y1);
-//echo "<p> calcula (x1: $x1 )  (y1: $y1 ) (z1: $z1 )";
-echo "<p>Passagem por valor: <br>",
-"calcula: ( x: $x1 ) ( y: $y1 ) ( z: $z1 )</p>";
-// resultado ( 2 ) ( 3 ) ( 7 )
+    $a = 4;
+    $a -= 2; // $a = $a - 2;
+    $a *= 2; // $a = $a * 2;
+    $a /= 2; // $a = $a / 2;
+    $a += 2; // $a = $a + 2;
 
-$x2 = 2;
-$y2 = 3;
-$z2 = calcula_ref($x2, $y2);
-echo "<p>Passagem por referência: <br>",
-"calcularef ( x: $x2 ) ( y: $y2 ) ( z: $z2 )</p>";
-// resultado ( x: 4 ) ( y: 3 ) ( z: 7 )
+    echo "Valor de \$a: ", $a . PHP_EOL;
+}
 
-$x3 = 2;
-$z3 = calcula_def($x3);
-echo "<p>Parâmetro default: <br>",
-"calcula_def ( x: $x3 ) ( z: $z3 )</p>";
-// resultado ( x: 4 ) ( z: 9 )
+/**
+ * Section: Demonstração do Uso de Funções
+ */
+function demonstrateFunctions(): void
+{
+    $x1 = 2;
+    $y1 = 3;
+    $z1 = calcular($x1, $y1);
+    //echo "<p> calcula (x1: $x1 )  (y1: $y1 ) (z1: $z1 )";
+    echo "<p>Passagem por valor: <br>",
+    "calcula: ( x: $x1 ) ( y: $y1 ) ( z: $z1 )</p>";
+    // resultado ( 2 ) ( 3 ) ( 7 )
 
-$a = 10;
-$b = 20;
-$x = 2;
-$y = 3;
-$z = calcula_local($x);
-echo "<p>Escopo local: <br>",
-"calcula_local ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
-// resultado ( 4 ) ( 3 ) ( 7 )
+    $x2 = 2;
+    $y2 = 3;
+    $z2 = calcula_ref($x2, $y2);
+    echo "<p>Passagem por referência: <br>",
+    "calcularef ( x: $x2 ) ( y: $y2 ) ( z: $z2 )</p>";
+    // resultado ( x: 4 ) ( y: 3 ) ( z: 7 )
 
-$a = 10;
-$b = 20;
-$x = 4;
-$z = calcula_global($x);
-echo "<p>Escopo global: <br>",
-"calcula_global ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
-// resultado ( 4 ) ( 3 ) ( 7 )
+    $x3 = 2;
+    $z3 = calcula_def($x3);
+    echo "<p>Parâmetro default: <br>",
+    "calcula_def ( x: $x3 ) ( z: $z3 )</p>";
+    // resultado ( x: 4 ) ( z: 9 )
 
-$x = 20;
-$y = 10;
-$z = calcula_static($x, $y);
-echo "<p>Escopo estático: <br>",
-"calcula_static #1 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+    $a = 10;
+    $b = 20;
+    $x = 2;
+    $y = 3;
+    $z = calcula_local($x);
+    echo "<p>Escopo local: <br>",
+    "calcula_local ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+    // resultado ( 4 ) ( 3 ) ( 7 )
 
-$z = calcula_static($x, $y);
-echo "<p>calcula_static #2 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+    $a = 10;
+    $b = 20;
+    $x = 4;
+    $z = calcula_global($x);
+    echo "<p>Escopo global: <br>",
+    "calcula_global ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+    // resultado ( 4 ) ( 3 ) ( 7 )
 
-$z = calcula_static($x, $y);
-echo "<p>calcula_static #3 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
-?>
+    $x = 20;
+    $y = 10;
+    $z = calcula_static($x, $y);
+    echo "<p>Escopo estático: <br>",
+    "calcula_static #1 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+
+    $z = calcula_static($x, $y);
+    echo "<p>calcula_static #2 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+
+    $z = calcula_static($x, $y);
+    echo "<p>calcula_static #3 ( a: $a ) ( b: $b ) ( x: $x ) ( z: $z )</p>";
+}
