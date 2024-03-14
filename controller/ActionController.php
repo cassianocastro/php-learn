eb0a41317846f00bb25c390e65b41cffeb6adf86
Cassiano Castro <development.castro@gmail.com>
2024-03-14 14:47:08 -0300

    Removed strings concatenation

--- controller/ActionController.php
+++ controller/ActionController.php
@@ -1,12 +1,16 @@
 <?php
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
+ini_set("display_errors", true);
+ini_set("display_startup_errors", true);
 error_reporting(E_ALL);
 
+/**
+ *
+ */
 function show()
 {
     echo "Hi " . htmlspecialchars($_POST['name']) . ".";
-    echo "You are " . (int) $_POST['age'] . " years old.";
+
+    echo "You are {$_POST['age']} years old.";
 }
 
 if ( isset($_POST["action"]) )
