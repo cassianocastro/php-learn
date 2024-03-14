f3e1642bd71287f412573f3611f5dbf1987ffd40
Cassiano Castro <development.castro@gmail.com>
2024-03-14 16:38:34 -0300

    File addition

---
+++ model/POO/Classes/ChildObject.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace model\POO\Classes;
+
+/**
+ *
+ */
+class ChildObject extends SimpleObject
+{
+
+}
+
+/**
+ *
+ */
+class ChildObject2 extends SimpleObject
+{
+
+}
