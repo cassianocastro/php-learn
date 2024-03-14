e324179a453f9ea37a8d23cc52ff690da6b82608
Cassiano Castro <development.castro@gmail.com>
2024-03-14 15:00:36 -0300

    Refactor: moved code inside of case blocks to separated functions

--- model/REST/rest.produto.php
+++ model/REST/rest.produto.php
@@ -1,4 +1,138 @@
 <?php
+
+/**
+ *
+ */
+function addProduct(): array
+{
+    global $conn;
+
+    $name   = $_REQUEST['produto_nome'];
+    $price  = $_REQUEST['produto_preco'];
+    $qty    = $_REQUEST['produto_qtd'];
+
+    $sucess = mysqli_query(
+        $conn,
+        <<<SQL
+            INSERT INTO
+                produto(produto_nome, produto_preco, produto_qtd)
+            VALUES
+                ($name, $price, $qty)
+        SQL
+    );
+
+    if ( $sucess )
+    {
+        return [
+            "status"  => "1",
+            "message" => "success",
+            "result"  => "produto add successfully"
+        ];
+    }
+
+    return [
+        "status"  => "1",
+        "message" => "success",
+        "result"  => "Something wrong!"
+    ];
+}
+
+/**
+ *
+ */
+function updateProduct(): array
+{
+    global $conn;
+
+    $id    = $_REQUEST['produto_id'];
+    $name  = $_REQUEST['produto_nome'];
+    $price = $_REQUEST['produto_preco'];
+    $qty   = $_REQUEST['produto_qtd'];
+
+    $success = mysqli_query(
+        $conn,
+        <<<SQL
+            UPDATE
+                produto
+            SET
+                produto_nome = $name, produto_preco = $price, produto_qtd = $qty
+            WHERE
+                produto_id = $id
+        SQL
+    );
+
+    if ( $success )
+    {
+        return [
+            "status"  => "1",
+            "message" => "success",
+            "result"  => "produto Update successfully"
+        ];
+    }
+
+    return [
+        "status"  => "1",
+        "message" => "success",
+        "result"  => "Something wrong!"
+    ];
+}
+
+/**
+ *
+ */
+function deleteProduct(): array
+{
+    global $conn;
+
+    $id = $_REQUEST['produto_id'];
+
+    $success = mysqli_query(
+        $conn,
+        "DELETE FROM produto WHERE produto_id = {$id}"
+    );
+
+    if ( $success )
+    {
+        return [
+            "status"  => "1",
+            "message" => "success",
+            "result"  => "produto Update successfully"
+        ];
+    }
+
+    return [
+        "status"  => "1",
+        "message" => "success",
+        "result"  => "Something wrong!"
+    ];
+}
+
+/**
+ *
+ */
+function getProducts(): array
+{
+    global $conn;
+
+    $result = mysqli_query($conn, "SELECT * from produto");
+
+    while ( $row = mysqli_fetch_row($result) )
+    {
+        $temp_cat[] = [
+            "produto_id"    => $row[0],
+            "produto_nome"  => $row[1],
+            "produto_preco" => $row[2],
+            "produto_qtd"   => $row[3]
+        ];
+    }
+
+    return [
+        "status"  => "1",
+        "message" => "success",
+        "result"  => $temp_cat
+    ];
+}
+
 try
 {
 	require_once __DIR__ . '/connection.php';
@@ -12,115 +146,16 @@ try
 		switch ( $method_name )
         {
 			case 'GET':
-				$result = mysqli_query($conn, "SELECT * from produto");
-
-				while ( $row = mysqli_fetch_row($result) )
-                {
-					$temp_cat[] = [
-						"produto_id"    => $row[0],
-						"produto_nome"  => $row[1],
-						"produto_preco" => $row[2],
-						"produto_qtd"   => $row[3]
-                    ];
-				}
-
-				$data = [
-					"status"  => "1",
-					"message" => "success",
-					"result"  => $temp_cat
-                ];
+				$data = getProducts();
 			    break;
 			case 'POST':
-				$name   = $_REQUEST['produto_nome'];
-				$price  = $_REQUEST['produto_preco'];
-				$qty    = $_REQUEST['produto_qtd'];
-
-                $sucess = mysqli_query(
-                    $conn,
-                    <<<SQL
-                        INSERT INTO
-                            produto(produto_nome, produto_preco, produto_qtd)
-                        VALUES
-                            ($name, $price, $qty)
-                    SQL
-                );
-
-				if ( $sucess )
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "produto add successfully"
-                    ];
-                }
-				else
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "Something wrong!"
-                    ];
-                }
+				$data = addProduct();
 			    break;
 			case 'PUT':
-				$id    = $_REQUEST['produto_id'];
-				$name  = $_REQUEST['produto_nome'];
-				$price = $_REQUEST['produto_preco'];
-				$qty   = $_REQUEST['produto_qtd'];
-
-				$success = mysqli_query(
-                    $conn,
-                    <<<SQL
-                        UPDATE
-                            produto
-                        SET
-                            produto_nome = $name, produto_preco = $price, produto_qtd = $qty
-                        WHERE
-                            produto_id = $id
-                    SQL
-                );
-
-				if ( $success )
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "produto Update successfully"
-                    ];
-                }
-				else
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "Something wrong!"
-                    ];
-                }
+                $data = updateProduct();
 			    break;
 			case 'DELETE':
-				$id = $_REQUEST['produto_id'];
-
-                $success = mysqli_query(
-                    $conn,
-                    "DELETE FROM produto WHERE produto_id = {$id}"
-                );
-
-				if ( $success )
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "produto Update successfully"
-                    ];
-                }
-				else
-                {
-					$data = [
-						"status"  => "1",
-						"message" => "success",
-						"result"  => "Something wrong!"
-                    ];
-                }
+                $data = deleteProduct();
 			    break;
 		}
 
