afcf8417e802d96ae2df6c62f3e5ee410fade04c
Cassiano Castro <development.castro@gmail.com>
2024-03-14 16:49:07 -0300

    Removed class definition: Template

--- model/POO/ConcreteClass/concreteClasses.php
+++ model/POO/ConcreteClass/concreteClasses.php
@@ -5,7 +5,6 @@ namespace model\POO\ConcreteClass;
 
 use Exception;
 use model\POO\AbstractClass\HTMLElement;
-use model\POO\Interfaces\iTemplate;
 
 /**
  *
@@ -111,35 +110,6 @@ class HTMLLinkElement extends HTMLBinaryElement
         $this->tag = "a";
         $this->conteudo = $conteudo;
         $this->url = $url;
-    }
-}
-
-/**
- *
- */
-class Template implements iTemplate
-{
-
-    private $vars;
-
-    public function __construct()
-    {
-        $this->vars = [];
-    }
-
-    public function setVariable($name, $var)
-    {
-        $this->vars[$name] = $var;
-    }
-
-    public function getHtml($template): string
-    {
-        foreach ( $this->vars as $name => $value )
-        {
-            $template = str_replace("\{$name\}", $value, $template);
-        }
-
-        return $template;
     }
 }
 
