21538fb554baac01c64cc8d6582c038363c7aa4b
Cassiano Castro <development.castro@gmail.com>
2024-03-14 16:26:57 -0300

    Class definition

---
+++ model/POO/AbstractClass/HTMLElement.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace model\POO\AbstractClass;
+
+use ReflectionClass;
+
+/**
+ *
+ */
+abstract class HTMLElement
+{
+
+    protected $tag;
+
+    public function __construct()
+    {
+        $this->tag = "";
+    }
+
+    protected function is_element($v): bool
+    {
+        if ( is_object($v) )
+        {
+            $r = new ReflectionClass(get_class($v));
+
+            return $r->isSubclassOf('HTMLElement');
+        }
+
+        return false;
+    }
+
+    abstract public function render(): string;
+}
