d2b2ecdb0ed74a1534849dbcd67a964054d5f418
Cassiano Castro <development.castro@gmail.com>
2024-03-14 16:44:28 -0300

    Removed class definition: ClasseConcreta1

--- model/POO/ConcreteClass/concreteClasses.php
+++ model/POO/ConcreteClass/concreteClasses.php
@@ -10,23 +10,6 @@ use model\POO\Interfaces\iTemplate;
 /**
  *
  */
-class ClasseConcreta1 extends ClasseAbstrata
-{
-
-    protected function pegarValor(): string
-    {
-        return "ClasseConcreta1<br>";
-    }
-
-    public function valorComPrefixo($prefixo): string
-    {
-        return "{$prefixo}ClasseConcreta1<br>";
-    }
-}
-
-/**
- *
- */
 class ClasseConcreta2 extends ClasseAbstrata
 {
 
