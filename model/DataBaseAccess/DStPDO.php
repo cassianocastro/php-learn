5f6c6ec77322b9e2bb1de558bcb454f0acd307cc
Cassiano Castro <development.castro@gmail.com>
2024-03-18 17:44:54 -0300

    Changed name of namespace

--- model/DataBaseAccess/DStPDO.php
+++ model/DataBaseAccess/DStPDO.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Model\DataBaseAcess;
+namespace Model\DataBaseAccess;
 
 use Exception, PDO;
 
--- model/DataBaseAccess/DbSingleton.php
+++ model/DataBaseAccess/DbSingleton.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Model\DataBaseAcess;
+namespace Model\DataBaseAccess;
 
 define("DB_USER", "root");
 define("DB_PASS", "");
