2d2d5050186aa20a9d5415aa91ef3d00f6edf7b7
Cassiano Castro <development.castro@gmail.com>
2024-03-20 12:29:52 -0300

    Defined function createTask()

--- public/pages/aulas_JS/.includes/aula_13/index.js
+++ public/pages/aulas_JS/.includes/aula_13/index.js
@@ -1,44 +1,54 @@
 "use strict";
 
+let count = 0;
+
+/**
+ *
+ */
+function createTask(content)
+{
+    const task  = document.createElement("div");
+    const icon  = document.createElement("button");
+    const check = document.createElement("input");
+    const p     = document.createElement("p");
+    const text  = document.createTextNode(content);
+
+    task.setAttribute("id", `task_${++count}`);
+    task.setAttribute("class", "task");
+
+    icon.setAttribute("type", "button");
+    icon.setAttribute("title", "Remove task");
+    icon.addEventListener('click', () => task.remove());
+    icon.classList.add("fas", "fa-trash-alt", "text-secondary");
+
+    check.setAttribute("type", "checkbox");
+    check.addEventListener('click', () => {
+        p.classList.toggle("complete");
+
+        if ( p.classList.contains("complete") )
+            p.textContent = `(concluido) ${p.textContent}`;
+        else
+            p.textContent = p.textContent.replace("(concluido) ", "");
+    });
+
+    p.appendChild(text);
+    task.append(icon, check, p);
+
+    return task;
+}
+
 /**
  *
  */
 function index()
 {
-    let count = 0;
-
     const input = document.querySelector("#nova-tarefa");
     const tasks = document.querySelector("#lista-tarefas");
 
     input.addEventListener("keypress", (e) => {
         if ( e.key === "Enter" )
         {
-            const task  = document.createElement("div");
-            const icon  = document.createElement("button");
-            const check = document.createElement("input");
-            const p     = document.createElement("p");
-            const text  = document.createTextNode(input.value);
-
-            task.setAttribute("id", `task_${++count}`);
-            task.setAttribute("class", "task");
-
-            icon.setAttribute("type", "button");
-            icon.setAttribute("title", "Remove task");
-            icon.addEventListener('click', () => task.remove());
-            icon.classList.add("fas", "fa-trash-alt", "text-secondary");
-
-            check.setAttribute("type", "checkbox");
-            check.addEventListener('click', () => {
-                p.classList.toggle("complete");
-
-                if ( p.classList.contains("complete") )
-                    p.textContent = `(concluido) ${p.textContent}`;
-                else
-                    p.textContent = p.textContent.replace("(concluido) ", "");
-            });
-
-            p.appendChild(text);
-            task.append(icon, check, p);
+            const task = createTask(input.value);
             tasks.appendChild(task);
 
             input.value = "";
